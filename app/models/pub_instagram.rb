915157e47d56e70237fb8cfde03a75626f80e089
tesstryan <tesstryan@gmail.com>
2014-05-22 21:10:16 -0500

    Updated PubInstagram Model

--- app/models/pub_instagram.rb
+++ app/models/pub_instagram.rb
@@ -1,4 +1,14 @@
 class PubInstagram
 
 
+  require 'open-uri'
+  require 'json'
+
+  def initialize
+    user_feed_url = 'https://api.instagram.com/v1/users/1175572646/media/recent/?access_token=' + ENV["INSTAGRAM_ACCESS"]
+    string_data = open(URI.encode(user_feed_url)).read
+    data = JSON.parse(string_data)
+
+  end
+
 end
